f0d906110d675c94309301c204017f7519f7d641
Jack He <jackhe2345@gmail.com>
2021-02-13 14:07:37 -0800

    added our home

---
+++ app.js
@@ -0,0 +1,67 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+const path = require('path');
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config();
+
+/**
+ * Controllers (route handlers).
+ */
+const checkBookController = require('./controllers/checkbook');
+
+/**
+ * Create Express server.
+ */
+
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+/**
+ * CheckBook API app routes.
+ */
+app.post("/api/checkbook/sendcheck", checkBookController.sendcheck)
+
+
+/**
+ * Error Handler.
+ */
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Server Error');
+  });
+}
+
+/**
+ * Start Express server.
+ */
+const port = 5000
+app.listen(port, () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), port, app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
+});
+
+module.exports = app;
---
+++ controllers/checkbook.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const axios = require('axios')
+/**
+ * POST /
+ * checkbook api
+ */
+exports.sendcheck = async (req, res, next) => {
+    const API_KEY = process.env.CHECK_BOOK_API_KEY; 
+    const API_SECRET = process.env.CHECK_BOOK_API_SECRET;
+    let info = req.body; 
+    console.log(info);
+    const headers = {
+        'Accept': 'application/json',
+        'Authorization': API_KEY + ":" + API_SECRET
+    }
+    if (!(info.recipient && info.name && info.amount && info.description)) {
+        res.status(400);
+        return res.json({
+            status: "failure", 
+            message: "insufficient information"
+        });
+    } 
+    const body = {
+        "recipient": info.recipient, 
+        "name": info.name, 
+        "amount": parseInt(info.amount), 
+        "description": info.description
+    }
+    try {
+        let response = await axios.post(String(process.env.CHECK_BOOK_SENDCHECK), body, { headers: headers });
+        res.json({
+            status: "success", 
+            message: response.data
+        })
+    } catch (err) {
+        res.json({
+            status: "failure", 
+            message: err
+        })
+    }
+};
+  
