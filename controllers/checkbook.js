7ac2d5adb09286ae26d2f6ac57a58c1f49bff785
Jack He <jackhe2345@gmail.com>
2021-02-14 08:54:49 -0800

    done checkbook

--- controllers/checkbook.js
+++ controllers/checkbook.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require('axios')
 /**
  * POST /
- * checkbook api
+ * sendcheck to some user
  */
 exports.sendcheck = async (req, res, next) => {
     const API_KEY = process.env.CHECK_BOOK_API_KEY; 
@@ -40,3 +40,44 @@ exports.sendcheck = async (req, res, next) => {
     }
 };
   
+
+
+/**
+ * POST /
+ * request payment from some user 
+ */
+exports.requestPayment = async (req, res, next) => {
+    const API_KEY = process.env.CHECK_BOOK_API_KEY; 
+    const API_SECRET = process.env.CHECK_BOOK_API_SECRET;
+    let info = req.body; 
+    console.log(info);
+    const headers = {
+        'Accept': 'application/json',
+        'Authorization': API_KEY + ":" + API_SECRET
+    }
+    if (!(info.recipient && info.name && info.amount && info.description)) {
+        res.status(400);
+        return res.json({
+            status: "failure", 
+            message: "insufficient information"
+        });
+    } 
+    const body = {
+        "recipient": info.recipient, 
+        "name": info.name, 
+        "amount": parseInt(info.amount), 
+        "description": info.description
+    }
+    try {
+        let response = await axios.post(String(process.env.CHECK_BOOK_SENDINVOICE), body, { headers: headers });
+        res.json({
+            status: "success", 
+            message: response.data
+        })
+    } catch (err) {
+        res.json({
+            status: "failure", 
+            message: err
+        })
+    }
+};
---
+++ models/HomeOwner.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+const homeOwnerSchema = new mongoose.Schema()
